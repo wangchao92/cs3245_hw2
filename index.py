efb5701d0f22273b814bab999899eea9295a8597
Sun Wang Jun <sunxperous@gmail.com>
2015-03-04 20:15:05 +0800

    initial commit

---
+++ index.py
@@ -0,0 +1,24 @@
+import argparse
+import nltk
+import os
+import sys
+
+def main():
+  # parser = argparse.ArgumentParser(prog='CS3245 HW2', description='CS3245 HW2')
+  # parser.add_argument('-i', required=True, help='directory-of-documents')
+  # parser.add_argument('-d', required=True, help='dictionary-file')
+  # parser.add_argument('-p', required=True, help='postings-file')
+
+  # args = parser.parse_args()
+  # directory_path = args.i
+  # dictionary_file_name = args.d
+  # postings_file_name = args.p
+
+  directory_path = 'test'
+  dictionary_file_name = 'dictionary.txt'
+  postings_file_name = 'postings.txt'
+
+  
+
+if __name__ == '__main__':
+  main()
