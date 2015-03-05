2924bdb63ffe2f71241880aec9ba9b2c27a015d6
bumbleblym <bumbleblym@gmail.com>
2015-03-06 01:35:21 +0800

    feat: negate/intersect

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import nltk
 import pickle
 import string
@@ -20,7 +21,8 @@ def main():
     query_file_name = args.q
     output_file_name = args.o
 
-    execute_queries(dictionary_file_name, postings_file_name, query_file_name, output_file_name)
+    execute_queries(
+        dictionary_file_name, postings_file_name, query_file_name, output_file_name)
 
 
 def execute_queries(dictionary_file_name='dictionary.txt', postings_file_name='postings.txt', query_file_name='query.txt', output_file_name='output.txt'):
@@ -28,12 +30,56 @@ def execute_queries(dictionary_file_name='dictionary.txt', postings_file_name='p
     with open(dictionary_file_name, 'r') as dictionary_file:
         ptr_dictionary = pickle.load(dictionary_file)
 
+    global universal_set
+    universal_set = ptr_dictionary['.']
+    global total_count
+    total_count = len(universal_set)
+
     query_file = open(query_file_name, 'r')
 
-    for query in query_file:
+    for line in query_file:
         pass
 
     query_file.close()
 
+
+def negate(postings_list):
+    result = [
+        doc_id for doc_id in universal_set if doc_id not in set(postings_list)]
+
+    return result
+
+
+def intersect(list_a, list_b, negate_a=False, negate_b=False):
+    # Optimise
+    if negate_a:
+        list_a = negate(list_a)
+
+    if negate_b:
+        list_b = negate(list_b)
+
+    sqrt_a = int(math.sqrt(len(list_a)))
+    sqrt_b = int(math.sqrt(len(list_b)))
+
+    result = []
+    i = j = 0
+    while i < len(list_a) and j < len(list_b):
+        if list_a[i] == list_b[j]:
+            result.append(list_a[i])
+            i += 1
+            j += 1
+        elif list_a[i] > list_b[j]:
+            if j % sqrt_b == 0 and j + sqrt_b < len(list_b) and list_a[i] >= list_b[j + sqrt_b]:
+                j += sqrt_b
+            else:
+                j += 1
+        else:
+            if i % sqrt_a == 0 and i + sqrt_a < len(list_a) and list_a[i + sqrt_a] <= list_b[j]:
+                i += sqrt_a
+            else:
+                i += 1
+
+    return result
+
 if __name__ == '__main__':
-    execute_queries()
+    print intersect([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 9])
